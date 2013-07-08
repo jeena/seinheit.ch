2156d3f5c6e9cf369b6aa75d51aa68833cd7323d
jeena <spam@jeenaparadies.net>
2013-07-08 22:40:09 +0200

    added admin password file

--- application/controller/AdminController.php
+++ application/controller/AdminController.php
@@ -12,7 +12,7 @@ class AdminController extends Katharsis_Controller_Abstract
 	
 	public function loginAction()
 	{
-		if($this->_getParam('password') == '')
+		if($this->_getParam('password') == $this->getPassword())
 		{
 			$_SESSION['logged'] = 1;
 			$this->_location('index');
@@ -28,5 +28,15 @@ class AdminController extends Katharsis_Controller_Abstract
 	{
 		$_SESSION['logged'] = 0;
 		$this->_location('gate');
+	}
+
+	// Private
+
+	private function getPassword()
+	{
+		$admin_ini = parse_ini_file('config/admin.ini');
+		$password = $admin_ini["password"];
+
+		return $password;
 	}
 }
