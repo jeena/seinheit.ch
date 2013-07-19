a85745747e39edd4645c93c469efedb8a76c469b
jeena <spam@jeenaparadies.net>
2013-07-19 15:34:33 +0200

    insert image

--- public/script/admin.js
+++ public/script/admin.js
@@ -3,6 +3,10 @@ function adminPageInsertImage(type, img) {
 	if(type == 'header') {
 		opener.document.forms[0].header_image.value = img;
 	} else {
-		opener.document.forms[0].content.value += '<img src="{@image ' + img + '}" alt="">';
+		var editor = opener.myCodeMirror;
+		var img = '<img src="{@image ' + img + '}" alt="">';
+		var pos = editor.getCursor() // or {line , ch };
+		var tok = editor.getTokenAt(pos);
+		editor.replaceRange(img, pos);
 	}
 }
